d7da9e7c6eb30dee052324c9faf521b0c802e33a
Yacine <Yacine22>
2024-02-23 19:44:55 +0100

    add versel host

--- cv/settings.py
+++ cv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4as0tpr5*dtcx+sx9s5ms-3k=k09n8d_ym@y*&&jwczxxvhw)^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.versel.app']
 
 
 # Application definition
